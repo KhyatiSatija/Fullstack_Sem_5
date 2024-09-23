385d22789ff6a8ea913dfafcc5726985e26781c1
KhyatiSatija <satijakhyati2003@gmail.com>
2024-09-23 20:05:21 +0530

    connected to mongoDB

--- express-crud/server.js
+++ express-crud/server.js
@@ -2,9 +2,21 @@ const express  = require("express")
 const app = express()
 
 
+const mongoose = require("mongoose");
+// URI or query string 
+// mongodb://username:password@URLOfMOngoDB(IP Address// even better -write the image name):portNumber/?finalQuery
+mongoose.connect(
+    "mongodb://root:root@mongo:27017/?authSource=admin")
+    .then( () => {
+        console.log("Successfully connected to MongoDB");
+    })
+    .catch((e) => {
+        console.log("Error trying to connect to MongoDB", e);
+    });
+
 // home page ka URL "/"
 app.get("/", (req, res) => {
-    res.send("<h1> Hello world using Express and Docker pom pom!!!</h1>");
+    res.send("<h1> Hello world using Express and Docker Compose!!!</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
